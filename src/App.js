cdf1d84a5d356cf3a0a959b27ec4fe65d7273dbe
jkhaha <jha2@wellesley.edu>
2019-04-13 12:47:46 -0400

    login/logout works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {appConfig} from './utils/constants';
+import { UserSession } from 'blockstack';
 
 class App extends Component {
+  state = {
+    userSession: new UserSession({appConfig})
+  };
+
+  componentDidMount = async () => {
+   const { userSession } = this.state
+
+   if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+     const userData = await userSession.handlePendingSignIn()
+
+     if (!userData.username) {
+       throw new Error('This app requires a username')
+     }
+     window.location = `/`
+   }
+ };
+
+  handleLogIn = () => {
+    const { userSession } = this.state
+    userSession.redirectToSignIn();
+  };
+
+  handleLogOut = () => {
+    const { userSession } = this.state
+  };
+
   render() {
+    const { userSession } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>HELLO BLOCKSTACK</p>
+        {
+          userSession.isUserSignedIn() ?
+          <button onClick={this.handleLogOut}> LOGOUT </button> : <button onClick={this.handleLogIn}> LOGIN </button>
+        }
       </div>
     );
   }
---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+import { AppConfig } from 'blockstack'
+
+export const appConfig = new AppConfig(['store_write', 'publish_data'])
+// might not need publish_data
+export const POST_FILENAME = 'posts.json'
