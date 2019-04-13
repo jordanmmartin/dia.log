954869f8cf6708d79777a79a4953bad9426c05a2
reduviidae <wiljago@gmail.com>
2019-04-13 19:56:27 -0400

    last weight added

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,7 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { MyContext } from './UserProvider'
 
 export default class Profile extends Component {
+  state = {
+    weights: []
+  }
+  fetchData = () => {
+    const userSession = this.context.state.currentUser.userSession;
+    userSession.getFile('weights.json', {decrypt: true})
+    .then((file) => {
+      let weights = JSON.parse(file || '[]');
+      console.log(weights);
+      this.setState({ weights })
+    })
+  }
+
+  componentDidMount () {
+    this.fetchData()
+  }
 
   render () {
     const { user } = this.props
@@ -9,6 +26,7 @@ export default class Profile extends Component {
     return (
       <div className="user-profile">
         <div>
+          <p className="centered">{this.state.weights ? `Your last weigh in was ${this.state.weights}lbs.` : `You don't have a previous weigh-in.`}</p>
           <p className="your-lessons">Your lessons</p>
           <p className="lesson">Eating Well to Prevent Type 2 Diabetes</p>
           <Link to='/chat'>
@@ -19,3 +37,5 @@ export default class Profile extends Component {
     )
   }
 }
+
+Profile.contextType = MyContext
