0406d5bd98355b28fb5da1ac07ef096485c5dba6
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2019-04-13 17:42:55 -0400

    added styling

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 export default class Profile extends Component {
   render () {
     return (
-      <div>
-        This is the profile page.
+      <div className="user-profile">
+        <div>
+          <p className="your-lessons">Your lessons</p>
+          <p className="lesson">Eating Well to Prevent Type 2 Diabetes</p><button className="secondary-button">Enter Lesson</button>
+        </div>
       </div>
     )
   }
