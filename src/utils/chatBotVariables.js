da5b8d14b6dd010ab260624562c37586c4448b66
jkhaha <jha2@wellesley.edu>
2019-04-13 17:33:51 -0400

    added timing

--- src/utils/chatBotVariables.js
+++ src/utils/chatBotVariables.js
@@ -78,25 +78,25 @@ const steps=[
             trigger: '3'
           },
           {
-            delay: 5000,
+            delay: 4000,
             id: "3",
             message: 'Let’s start by talking about how to eat well to lower your risk of type 2 diabetes. First of all, you’ll want to choose items that are low in calories. Calories are a measure of energy. You get them from things you eat and drink. Your body burns calories the way a car burns gas. If you want to lose one pound per week, you’ll need to cut about 3,500 calories each week.',
             trigger: '4'
           },
           {
-            delay: 3000,
+            delay: 6000,
             id: '4',
             message: 'At the same time, you’ll want to choose items that are low in fat. Why would you want to choose items that are low in fat? Fat is high in calories. Plus, fats that are solid at room temperature can harm your heart.',
             trigger: '5'
           },
           {
-            delay: 5000,
+            delay: 4000,
             id: '5',
             message: 'You’ll also want to choose items that are low in sugar. Why would you want to choose items that are low in sugar? Sweet foods can be high in calories, although not as high as fatty foods. (1 tablespoon white sugar has 48 calories. 1 tablespoon butter has 102 calories.) Plus, sweet foods make your blood sugar go up.',
             trigger: 'quiz-1'
           },
           {
-            delay: 3000,
+            delay: 6000,
             id: 'quiz-1',
             message: "How many calories do you have to burn to lose one pound?",
             trigger: 'quiz-1-options'
@@ -117,8 +117,50 @@ const steps=[
           {
             id: 'correct',
             message: "That's correct! Let's move on.",
+            trigger: '6'
+          },
+          {
+            delay: 3000,
+            id: '6',
+            message:'At the same time, you’ll want to choose items that are high in fiber and water. Fiber is a type of carbohydrate that passes through your body without being digested.',
+            trigger: '7'
+          },
+          {
+            delay: 5000,
+            id: '7',
+            message: 'And why would you want to choose items that are high in fiber and water? They fill you up without adding calories.',
+            trigger: '8'
+          },
+          {
+            delay: 4000,
+            id: '8',
+            message: 'Finally, you’ll want to choose items that are high in vitamins, minerals, and protein. Why would you want to choose items that are high in vitamins, minerals, and protein? Your body needs them to be healthy. And many of us don’t get enough of them.',
+            trigger: 'quiz-2'
+          },
+          {
+            delay: 6000,
+            id: 'quiz-2',
+            message: "To prevent type 2 diabetes, you want to limit food items high in:",
+            trigger: 'quiz-2-options'
+          },
+          {
+            id: 'quiz-2-options',
+            options: [
+              {value: 2000, label: 'calories', trigger: 'correct-2'},
+              {value: 1500, label: 'fiber and water', trigger: 'wrong-2'},
+              {value: 3500, label: 'vitamins, minerals, and protein', trigger: 'wrong-2'}
+            ]
+          },
+          {
+            id: 'wrong-2',
+            message: 'Wrong answer, try again.',
+            trigger: 'quiz-2-options'
+          },
+          {
+            id: 'correct-2',
+            message: "That's correct! Let's move on.",
             end: true
-          }
+          },
         ]
 
 export default steps;
